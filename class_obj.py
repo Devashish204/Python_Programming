dd9d9ab8018109fb370bd685e82402d05a47b363
Devashish Rajput <109453645+Devashish204@users.noreply.github.com>
2024-06-21 10:16:23 +0530

    Add files via upload

---
+++ class_obj.py
@@ -0,0 +1,16 @@
+#creating class
+class Student:
+    name = "Devashish"
+
+#creating object(instance)
+
+s1 = Student()
+print(s1.name)
+
+class Car:
+    color="Black"
+    brand = "mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
